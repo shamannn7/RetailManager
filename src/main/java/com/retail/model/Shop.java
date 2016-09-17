98c5fd58daa5d351307a675e2eb5f393044f42ef
shamannn7 <razoryonov@gmail.com>
2016-09-17 15:18:25 +0100

    extending shop and adding shop list

--- src/main/java/com/retail/model/Shop.java
+++ src/main/java/com/retail/model/Shop.java
@@ -2,9 +2,16 @@ package com.retail.model;
 
 public class Shop {
     private String name;
-    private String number;
+    private String number;//TODO consider separating address entity
     private String postCode;
+    private String longitude;
+    private String latitude;
 
+    public Shop(String name, String number, String postCode) {
+        this.name = name;
+        this.number = number;
+        this.postCode = postCode;
+    }
 
     public String getName() {
         return name;
@@ -16,5 +23,21 @@ public class Shop {
 
     public String getPostCode() {
         return postCode;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
     }
 }
--- src/test/java/com/retail/service/ShopServiceTest.java
+++ src/test/java/com/retail/service/ShopServiceTest.java
@@ -1,15 +1,24 @@
 package com.retail.service;
 
 
+import com.retail.model.Shop;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ShopServiceTest {
+    private ShopService uut;
+
+    @Before
+    public void setUp(){
+        uut = new ShopService();
+    }
 
     @Test
     public void test_Add(){
-
+        Shop shop = new Shop("Westfield","22", "E161QQ");
+        uut.add(shop);
     }
 }
