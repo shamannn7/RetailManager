90281132783461c2354f358f5cf908a911af8de1
shamannn7 <razoryonov@gmail.com>
2016-09-17 15:18:05 +0100

    extending shop and adding shop list

--- src/main/java/com/retail/service/ShopService.java
+++ src/main/java/com/retail/service/ShopService.java
@@ -1,4 +1,14 @@
 package com.retail.service;
 
-public class ShopService {
+import com.retail.model.Shop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ShopService {
+    private List<Shop> shops = new ArrayList<>();
+
+    void add(Shop shop){
+        shops.add(shop);
+    }
 }
