7866d1ef57090d992a52d4f58fc043c25ff2287f
shamannn7 <razoryonov@gmail.com>
2016-09-18 19:39:05 +0100

    wiremock is needed only for tests, otherwise it clashes with tomcat

--- src/main/java/com/retail/ShopApiApplication.java
+++ src/main/java/com/retail/ShopApiApplication.java
@@ -3,10 +3,11 @@ package com.retail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ShopApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApiApplication.class, args);
+    }
 }
