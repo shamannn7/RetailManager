dc0e5212668700da575f3fc0e621699006f05c5d
shamannn7 <razoryonov@gmail.com>
2016-09-18 19:17:04 +0100

    renaming

--- src/test/java/com/retail/ShopApiTests.java
+++ src/test/java/com/retail/ShopApiTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RetailManagerApplicationTests {
+public class ShopApiTests {
 
 	@Test
 	public void contextLoads() {
